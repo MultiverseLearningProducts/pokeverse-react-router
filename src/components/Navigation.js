44788e73de4a9c5fb6f98e41173e3ae31819ef73
Adam Sisk <adamsisk91@gmail.com>
2022-06-02 22:18:26 -0500

    adds squirtle to nav

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,12 +2,17 @@ import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Container from 'react-bootstrap/Container';
+import Image from 'react-bootstrap/Image';
+import squirtle from '../../public/squirtle.png';
 
 function Navigation() {
   return (
     <Navbar sticky='top' bg='dark' variant='dark' className="mb-4">
       <Container>
-        <Navbar.Brand>Pokeverse</Navbar.Brand>
+        <Navbar.Brand>
+          <Image src={squirtle} width="30" className="me-2" />
+          Pokeverse
+        </Navbar.Brand>
         <Nav className='me-auto'>
           <Nav.Link href='/'>All Pokemon</Nav.Link>
         </Nav>
